793b43779231a42bd7c1d1a8b1970c7d58d972f4
Tashrik Anam <anampartho@users.noreply.github.com>
2024-08-23 21:21:54 +0600

    feat: setup redux toolkit

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,8 +1,13 @@
 import { FC, PropsWithChildren } from 'react';
 import ReactQueryProvider from './ReactQueryProvider';
+import ReduxProvider from './ReduxProvider';
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
-  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+  return (
+    <ReduxProvider>
+      <ReactQueryProvider>{children}</ReactQueryProvider>
+    </ReduxProvider>
+  );
 };
 
 export default Providers;
